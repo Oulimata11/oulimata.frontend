49ee85bd1efb8ff72346298cc4c555ba2019095d
Oulimata11 <seneoulymata04@gmail.com>
2023-07-09 19:48:28 +0000

    hh

--- src/app/home/utilisateur/list-utilisateur/list-utilisateur.component.ts
+++ src/app/home/utilisateur/list-utilisateur/list-utilisateur.component.ts
@@ -8,8 +8,11 @@ import { ApiService } from 'app/service/api/api.service';
 })
 export class ListUtilisateurComponent {
   loading_get_utilisateur = false
+  loading_desactiver_compte =false
+  loading_activer_compte =false
   selected_utilisateur: any = undefined
   utilisateur_to_edit: any = undefined
+  user_disable :any =undefined
   constructor(public api: ApiService,private modalService:NgbModal) {
 
   }
@@ -58,10 +61,61 @@ export class ListUtilisateurComponent {
       centered: true
     });
   }
-  open_modal_edit(modal_edit, un_utilisateur: any) {
-    this.utilisateur_to_edit = un_utilisateur;
-    this.modalService.open(modal_edit, {
+  open_modal_desactiver_compte(modal_desactiver_compte, user: any) {
+    this.user_disable = user;
+    this.modalService.open(modal_desactiver_compte, {
       centered: true,
     });
+  }
+  //Désactiver un compte
+  desactiver_compte() {
+    this.loading_desactiver_compte = true;
+    this.api.taf_post(
+      "utilisateur/disable_user",
+      { id_utilisateur: this.user_disable.id_utilisateur },
+      (reponse: any) => {
+        this.loading_desactiver_compte  = false;
+        if (reponse.status) {
+          console.log("Opération effectuée avec succés sur la table utilisateur. Réponse= ",reponse);
+          this.api.Swal_success("Compte désactivé avec succés!");
+          this.get_utilisateur();
+          this.modalService.dismissAll();
+        } else {
+          console.log(
+            "L'opération sur la table utilisateur a échoué. Réponse= ",
+            reponse
+          );
+          alert("La tentative de désactivation du compte a echoué");
+        }
+      },
+      (error: any) => {
+        this.loading_desactiver_compte = false;
+      }
+    );
+  }
+  //Activer un compte
+  activer_compte(user_enable: any) {
+    this.loading_activer_compte = true;
+    this.api.taf_post(
+      "utilisateur/enable_user",
+      { id_utilisateur: user_enable.id_utilisateur },
+      (reponse: any) => {
+        this.loading_activer_compte  = false;
+        if (reponse.status) {
+          console.log("Opération effectuée avec succés sur la table utilisateur. Réponse= ",reponse);
+          this.api.Swal_success("Compte activé avec succés!");
+          this.get_utilisateur();
+        } else {
+          console.log(
+            "L'opération sur la table utilisateur a échoué. Réponse= ",
+            reponse
+          );
+          alert("La tentative d'activation du compte a echoué");
+        }
+      },
+      (error: any) => {
+        this.loading_activer_compte = false;
+      }
+    );
   }
 }
